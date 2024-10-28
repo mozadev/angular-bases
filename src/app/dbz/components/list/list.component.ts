f6da8e1dab24111dd65220075a827aedb800ab89
mozadev <ceosmore@gmail.com>
2024-10-28 18:00:41 -0500

    create component list and addCharacter

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -3,10 +3,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-dbz-list',
-    standalone: true,
-    imports: [
-        CommonModule,
-    ],
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.css'],
 })
