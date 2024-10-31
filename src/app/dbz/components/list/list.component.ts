2461e0d4b7631afe27d9e1d3a91583970a0cb87b
mozadev <ceosmore@gmail.com>
2024-10-31 17:53:09 -0500

    Add services , uuid package, private service

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -12,11 +12,23 @@ export class ListComponent {
     // to get a property called characterList from father (main-page-component) if you don't define inside ()
     @Input() 
     public characterList: Character[]= [
-        // {
-        //     name:'Trunk',
-        //     power: 10
-        // }
+        {
+            name:'Trunk',
+            power: 10
+        }
     ]
+    // enviando al padre
+    @Output()
+    public onDelete: EventEmitter<string> = new EventEmitter();
+
+    onDeleteCharacter(id?: string): void {
+        // emitir el ID del personaje
+        if(!id ) return;
+        
+        console.log({id})
+
+        this.onDelete.emit(id);
+    }
  }
 
 
